ae03d9ed702a1a25b9b3543fbc8eb843cf41abb2
SUGAN S <sugangoldenfish@gmail.com>
2025-06-17 20:37:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ import numpy as np
 def create_connection():
     try:
         connection = pymysql.connect(
-            host='localhost',
+            host='127.0.0.1',
             user='root',
             password='12345',
-            db='police_log',
-            port=3307,
+            database='police_log',
+            port=3306,
             cursorclass=DictCursor
         )
         return connection
