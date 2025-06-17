b89f9b9cbc8c19e77ba1915d4ad1d4d012c3eeeb
SUGAN S <sugangoldenfish@gmail.com>
2025-06-17 18:57:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import numpy as np
 def create_connection():
     try:
         connection = pymysql.connect(
-            host='localhost',
+            host='LAPTOP-22PMNJAD',
             user='root',
             password='12345',
             db='police_log',
